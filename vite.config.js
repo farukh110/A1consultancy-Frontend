663bbc7c35001d352cb6ff2ed6e0a6f3e0620254
farukh110 <farukhsajjad110@gmail.com>
2023-12-10 18:38:38 +0500

    applied some things

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "./runtimeConfig": "./runtimeConfig.browser",
-    },
-  },
+  base: "A1consultancy-Frontend"
 })
